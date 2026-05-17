5ba97bd0e8580a8ef2e0d3d302bfc348305fba6e
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2026-05-17 17:41:34 -0300

    ✨ feat(Button): Adiciona manipulador de evento onClick
    Move o `onClick` do elemento `button` para o local correto, associando-o à função passada via `props.funcao`. Isso garante que o evento de clique do botão agora funcione como esperado.

--- src/assets/components/Button.jsx
+++ src/assets/components/Button.jsx
@@ -1,8 +1,7 @@
 export default function Button(props) {
     return (
-        <button>
-            onClick={props.funcao}
+        <button onClick={props.funcao}>
             {props.btnText}
         </button>
-    )   
+    );
 }
